fa4124428aa6e047a189762dfb94255c6d3b9576
hlmasterchief <hlmasterchief93@gmail.com>
2020-05-14 13:31:51 +0300

    list v1

--- index.js
+++ index.js
@@ -94,5 +94,54 @@ const argv = yargs
         });
     }
   })
+
+  .command({
+    command: 'list [folder] [output]',
+    desc: 'List images and save to a file',
+    builder: (yargs) => {
+      yargs
+        .positional('folder', {
+          desc: 'Image folder',
+          default: '.',
+          type: 'string'
+        })
+        .option('output', {
+          alias: 'o',
+          desc: 'Output file name',
+          default: `${Date.now()}.txt`,
+          type: 'string'
+        })
+        .example('$0 img -o out.txt', 'List images in img/ and save to img/out.txt')
+    },
+    handler: (argv) => {
+      console.log(`folder: ${argv.folder} | output: ${argv.output}`);
+
+      const dirPath = argv.folder;
+      const filePath = `${argv.folder}/${argv.output}`;
+
+      if (!fs.existsSync(dirPath) || !fs.lstatSync(dirPath).isDirectory()) {
+        return console.log('Not a folder');
+      }
+
+      if (fs.existsSync(filePath)) {
+        return console.log('File exist');
+      }
+
+      const stream = fs.createWriteStream(filePath);
+      stream.on('error', console.error);
+
+      const imgPath = `${argv.folder}/*.{jfif,jpeg,jpg,png,gif}`;
+      glob(imgPath, async (err, files) => {
+        if (err) return console.error(err.message);
+
+        for (const file of files) {
+          let line = '';
+          line += file.substring(file.lastIndexOf('/') + 1);
+          stream.write(line + '\n');
+        }
+      });
+    }
+  })
+
   .alias('help', 'h')
   .argv;
